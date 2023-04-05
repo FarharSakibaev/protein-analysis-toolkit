74d838dcccf2f79873142cb909366ee95611f520
sakibaev <farkhat.sakibaev@bostongene.com>
2023-04-06 01:34:18 +0400

    Checking local version by git

--- setup.py
+++ setup.py
@@ -10,18 +10,11 @@ def setup():
         update()
 
     dirs_to_create = ['input', 'output']
-    files_to_create = ['version.txt']
 
     for dir_path in dirs_to_create:
         dir_path = f'{PROJECT_PATH}/{dir_path}'
         if not os.path.exists(dir_path):
             os.mkdir(dir_path)
-
-    for file_path in files_to_create:
-        file_path = f'{PROJECT_PATH}/{file_path}'
-        if not os.path.exists(file_path):
-            with open(file_path, 'w') as file:
-                file.write('')
 
 
 if __name__ == '__main__':
--- update.py
+++ update.py
@@ -1,8 +1,7 @@
 import subprocess
+from datetime import datetime, timedelta
 
 import requests
-
-from config import PROJECT_PATH
 
 
 def git_is_installed() -> bool:
@@ -15,14 +14,29 @@ def git_is_installed() -> bool:
         return False
 
 
+def get_last_commit() -> dict:
+    try:
+        output = subprocess.check_output(['git', 'log', '-1'], stderr=subprocess.STDOUT)
+        output = output.decode('utf-8').split('\n')
+
+        last_version = output[0].replace('commit', '').strip()
+        date = output[2].replace('Date:', '').strip()
+
+        return {
+            'local_version': last_version,
+            'date': datetime.strptime(date[:-6], "%a %b %d %H:%M:%S %Y")
+        }
+    except subprocess.CalledProcessError as e:
+        print("Error:", e.output)
+
+
 def update() -> None:
     if not git_is_installed():
         return
 
-    file_path = f'{PROJECT_PATH}/version.txt'
-
-    with open(file_path, 'r') as version_file:
-        version = version_file.read().strip().replace('\n', '')
+    local_version_data = get_last_commit()
+    delta = datetime.now() - local_version_data['date']
+    version = local_version_data['local_version']
 
     owner = 'FarharSakibaev'
     repo = 'protein-analysis-toolkit'
@@ -32,12 +46,9 @@ def update() -> None:
 
     latest_version = latest_commit['sha']
 
-    if version != latest_version or latest_version not in version:
+    if delta < timedelta(days=1) and version != latest_version or latest_version not in version:
         subprocess.run(["git", "checkout", "master"])
         subprocess.run(["git", "pull"])
-
-        with open(file_path, 'w') as version_file:
-            version_file.write(latest_version)
         exit('Project updated, please run the application again')
     else:
         print('Nothing to update')
