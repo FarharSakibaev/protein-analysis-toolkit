5624469e13f0ccb91821ba1952f4f721f3f856ef
sakibaev <farkhat.sakibaev@bostongene.com>
2023-04-05 19:58:57 +0400

    Updates beta implemented

--- setup.py
+++ setup.py
@@ -5,11 +5,17 @@ from config import PROJECT_PATH
 
 def setup():
     dirs_to_create = ['input', 'output']
+    files_to_create = ['version.txt']
 
     for dir_path in dirs_to_create:
         dir_path = f'{PROJECT_PATH}/{dir_path}'
         if not os.path.exists(dir_path):
             os.mkdir(dir_path)
+
+    for file_path in files_to_create:
+        file_path = f'{PROJECT_PATH}/{file_path}'
+        with open(file_path, 'w') as file:
+            file.write('')
 
 
 if __name__ == '__main__':
--- update.py
+++ update.py
@@ -1,7 +1,28 @@
+import subprocess
+
 import requests
 
+from config import PROJECT_PATH
 
-if __name__ == '__main__':
+
+def git_is_installed() -> bool:
+    try:
+        result = subprocess.run(['git', '--version'], capture_output=True, text=True)
+        print(result.stdout)
+        return True
+    except FileNotFoundError:
+        print('Please install Git on your PC if you want to fetch updates')
+        return False
+
+
+def update() -> None:
+    if not git_is_installed():
+        return
+
+    file_path = f'{PROJECT_PATH}/version.txt'
+
+    with open(file_path, 'r') as version_file:
+        version = version_file.read().strip().replace('\n', '')
 
     owner = 'FarharSakibaev'
     repo = 'protein-analysis-toolkit'
@@ -9,4 +30,18 @@ if __name__ == '__main__':
     response = requests.get(f'https://api.github.com/repos/{owner}/{repo}/commits/master')
     latest_commit = response.json()
 
-    print(latest_commit['sha'])
+    latest_version = latest_commit['sha']
+
+    if version != latest_version or latest_version not in version:
+        subprocess.run(["git", "checkout", "master"])
+        subprocess.run(["git", "fetch"])
+
+        with open(file_path, 'w') as version_file:
+            version_file.write(latest_version)
+        print('Project updated')
+    else:
+        print('Nothing to update')
+
+
+if __name__ == '__main__':
+    update()
